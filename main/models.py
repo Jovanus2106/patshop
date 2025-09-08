70d41d725226bfe0323cb9d4b970721b05be2b14
Jovanus Irwan <jovanusirwan@gmail.com>
2025-09-08 22:41:46 +0700

    final beneran

--- main/models.py
+++ main/models.py
@@ -10,13 +10,22 @@ class Product(models.Model):
     ('SPORTS', 'Olahraga')]
 
 
+class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('ELECTRONICS', 'Elektronik'),
+        ('FASHION', 'Fashion'),
+        ('HOME', 'Peralatan Rumah'),
+        ('FOOD', 'Makanan & Minuman'),
+        ('SPORTS', 'Olahraga'),
+    ]
+
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     description = models.TextField()
     thumbnail = models.URLField()
-    category = models.CharField(max_length=50)
-    is_featured = models.BooleanField(default=False)
-    stock = models.IntegerField(default=0)
+    category = models.CharField(max_length=20,choices=CATEGORY_CHOICES)
+    stock = models.IntegerField()
+
     
     def __str__(self):
         return self.name
